ce27c9ca8ee3837ae17846a3118557ce5d6c433c
Jaime Pineda <jaime.e.pineda@gmail.com>
2015-12-07 14:34:43 +0100

    Adds figure for different rms
    -PNG files for fit
    -comparison of results for two levels of the rms

--- fit_lineprofilesH.py
+++ fit_lineprofilesH.py
@@ -11,6 +11,7 @@ file_in='data/Core2_N2Hp_10.fits'
 freq_line=93173.3922e6*u.Hz
 
 file_thick='fits/Core2_N2Hp_thick_fitted_parameters_snr3.fits'
+file_thick_v2='fits/Core2_N2Hp_thick_fitted_parameters_snr3_v2.fits'
 snr_min = 3.
 
 cube = pyspeckit.Cube(file_in)
@@ -36,7 +37,7 @@ plt.ion()
 
 cube.Registry.add_fitter('n2hp_vtau', pyspeckit.models.n2hp.n2hp_vtau_fitter, 4)
 
-print('start optically thick fit')
+print('start optically thick fit without error')
 cube.fiteach(fittype='n2hp_vtau',  guesses=[5, 0.07, 6.7, 0.3], 
              verbose_level=2, signal_cut=snr_min,
              limitedmax=[F,T,T,T],
@@ -48,10 +49,28 @@ cube.fiteach(fittype='n2hp_vtau',  guesses=[5, 0.07, 6.7, 0.3],
              position_order = 1/peaksnr,
              errmap=rms_map, 
              multicore=multicore)
-
 cube.write_fit( file_thick, clobber=True)
 
+print('start optically thick fit with error')
+# cube.error[:] = rms_map
+cube.fiteach(fittype='n2hp_vtau',  guesses=[5, 0.07, 6.7, 0.3], 
+             verbose_level=2, signal_cut=snr_min,
+             limitedmax=[F,T,T,T],
+             limitedmin=[T,T,T,T],
+             minpars=[ 0,  0,vmin,0.05],
+             maxpars=[20.,50,vmax,1.0],
+             fixed=[F,F,F,F], 
+             use_neighbor_as_guess=True, 
+             position_order = 1/peaksnr,
+             errmap=rms_map*0.5, 
+             multicore=multicore)
+
+cube.write_fit( file_thick_v2, clobber=True)
+
+# plot figure
 cube.Registry.add_fitter('n2hp_vtau', pyspeckit.models.n2hp.n2hp_vtau_fitter, 4)
+
+# No error
 cube.load_model_fit( file_thick, npars=4, npeaks=1, _temp_fit_loc=(xmax,ymax))
 cube.mapplot()
 cube.plot_spectrum(xmax,ymax, plot_fit=True)
@@ -60,3 +79,15 @@ cube.mapplot(estimator=None, vmin=2, vmax=5)
 plt.draw()
 plt.show()
 
+plt.savefig('N2Hp_pyspeckit_fit_from_cube_with_error.png')
+
+# With error
+cube.load_model_fit( file_thick_v2, npars=4, npeaks=1, _temp_fit_loc=(xmax,ymax))
+cube.mapplot()
+cube.plot_spectrum(xmax,ymax, plot_fit=True)
+cube.mapplot.plane = cube.parcube[0,:,:]
+cube.mapplot(estimator=None, vmin=2, vmax=5)
+plt.draw()
+plt.show()
+
+plt.savefig('N2Hp_pyspeckit_fit_from_cube_with_half_error.png')
